8d1270e3edab006093d66c02d72e242c7f281d14
Jhauanio <jonah.hauanio@gmail.com>
2022-05-05 10:29:33 -0700

    final

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const body = document.querySelector('body')
 
 
 //declares an array containing img links
-const imgSrc = ['https://i.kym-cdn.com/photos/images/newsfeed/001/431/201/40f.png', 'https://images.gamebanana.com/img/ico/sprays/5de275cbb3314.png', 'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg', 'https://pbs.twimg.com/media/EUTJiL5UYAAPM0S?format=png&name=small'];
+const imgSrc = ['https://i.kym-cdn.com/photos/images/newsfeed/001/431/201/40f.png', 'https://images.gamebanana.com/img/ico/sprays/5de275cbb3314.png', 'https://i.kym-cdn.com/entries/icons/facebook/000/027/852/Screen_Shot_2018-12-12_at_1.02.39_PM.jpg', 'https://pbs.twimg.com/media/EUTJiL5UYAAPM0S?format=png&name=small', 'https://i.kym-cdn.com/photos/images/facebook/000/325/428/264.jpg'];
 
 //creates function that randomly generates image
 const ranImgGen = () => {
@@ -17,7 +17,15 @@ const ranImgGen = () => {
 }
 
 //assign background image to randomly generated image
-body.style.backgroundImage = ranImgGen(); 
+body.style.backgroundImage = ranImgGen();
+const interval = window.setInterval(function(){
+    if(body.style.backgroundImage !== ''){
+        body.style.backgroundImage = '';
+    }else{
+        body.style.backgroundImage = ranImgGen();
+    }
+}, 500);
+
 
 
 //select all text elements
@@ -56,12 +64,12 @@ links.forEach((link) => {
             link.style.position = 'absolute'            
             link.style.top = `${Math.random() * 100}px`
             link.style.left = `${Math.random() * 100}px`
-            link.preventDefault();
             // link.style.pointerEvents = 'none';
             tracker--;
         } else if (tracker === 0){
             link.setAttribute('href', linkStorage.pop());
         }
+        console.log(tracker)
     });
 });
 
@@ -79,35 +87,8 @@ links.forEach((link) => {
 
 
 
-// rick roll
-//play rick in the background
-
-// const rr = new Audio('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-// // rr.setAttribute('loop', true);
-// rr.setAttribute('id', "rickrolling");
-// body.appendChild(rr);
-// rr.autoplay = true;
-// rr.loop = true;
-
-
-//{/* <iframe src=”https://olafwempe.com/mp3/silence/silence.mp3″ type=”audio/mp3″ allow=”autoplay” id=”audio” style=”display:none”></iframe>
-//<audio autoplay>
-//<source src=”https://olafwempe.com/mp3/Septium.mp3″ type=”audio/mp3″>
-//</audio> */}//
-     
-//var myAudio = new Audio(chrome.runtime.getURL("path/to/file.mp3"));
-//myAudio.play();
-
-    // const rrmusic = document.createElement('script');
-    // const rmusicBlock = document.createElement('audio');
-    // rmusicBlock.setAttribute('id', 'rickRoll');
-    // rmusicBlock.setAttribute('src', 'assets/rr.mp3');
-
-    //<script>
-
-
-
-// links move 
+// rick roll (DONE)
+// links move (DONE)
     // cache number of clicks
     // after a click move link 
     // after  # of clicks is > cache stop moving
@@ -115,3 +96,16 @@ links.forEach((link) => {
 // scrolling wrong
 // buttons don't work
 // images flashing at just slow enough not to cause a seizure
+
+//add popup
+
+const popUpTimer = () => {setTimeout(() => {
+    const popup = window.confirm('LIMITED TIME OFFER: CODESMITH FULL TUITION SCHOLARSHIP!!! LIMITED TIME!!!');
+    if (popup){
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+    } else {popUpTimer()}
+}, 5000)};
+
+popUpTimer()
+
+
